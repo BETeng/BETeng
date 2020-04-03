11af296e2f281cf64ab2dfe36f164655addc8d79
LG BeTeng <brian7.teng@gmail.com>
2020-04-02 22:37:56 -0500

    working hook form

--- yelp-hook-form/src/App.js
+++ yelp-hook-form/src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hi world</h1>
-    </div>
-  );
+const App = () => {
+    const key = process.env.REACT_APP_API_KEY;
+
+
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+
+    }, [])
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+    }
+
+    const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+        console.log(search);
+
+    }
+
+
+    return (
+        <div className="App">
+            <form className="search-form" onSubmit={getSearch}>
+                <input className="search-bar" type="text"
+                    value={search}
+                    onChange={updateSearch}
+                />
+                <button className="search-button"
+                    type="submit">
+                    Search
+            </button>
+            </form>
+
+        </div>
+    )
 }
 
 export default App;
